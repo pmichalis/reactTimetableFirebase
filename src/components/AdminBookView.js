f842a5886f0ec658780e559b916c7d53fb65ca56
pmichalis <piotrmichalis@gmail.com>
2019-01-21 22:30:20 +0100

    working accordion

--- src/components/AdminBookView.js
+++ src/components/AdminBookView.js
@@ -2,56 +2,55 @@ import React from 'react';
 import newId from './newid';
 import {connect} from 'react-redux';
 import {sendBookToEditAction} from '../store/actions';
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemTitle,
+    AccordionItemBody,
+} from 'react-accessible-accordion';
+import 'react-accessible-accordion/dist/fancy-example.css';
 
 
 class BookView extends React.Component {
 
-    componentWillMount() {
-        this.id = newId();
-    }
-
-    
-    render() {
+componentWillMount() {
+    this.id = newId();
+}
+   
+render() {
         return (
-
-            
             <div className="orderView row adminRecipeAdd tgrecipe">
-               
-                <div class="col-md-12 recipebody ">
-                <div class="headerpercipe">
-                   <div class="col-md-10 btrecipeadmin">
-                   <h4>{this.id}. {this.props.book.name}</h4></div>
-                    <div class="col-md-2 btrecipeadmin">
-                    <i>Czas: {this.props.book.time} min</i></div>
-                </div>
-                <div>
-                    <div class="col-md-4 btrecipeadmin">
-                    {this.props.book.image}</div>
-                    <div class="col-md-8 btrecipeadmin">
-                    <strong>Składniki</strong><br/>
-                    <div class="descriptioncomponents">
-                    {this.props.book.componentsRecipe}</div>
-                    </div>
+               <Accordion>
+                <AccordionItem>
+                    <AccordionItemTitle>
+                        <div>
+                        <div class="col-md-8 btrecipeadmin"><strong>{this.id}. {this.props.book.name}</strong></div>
+                        <div class="col-md-2 btrecipeadmin">{this.props.book.time} minut</div>
+                        <div className="col-xs-1 btrecipeadmin">
+                            <button className="btn btn-danger" onClick={ (event) => this.props.sendBookToEdit(this.props.book)}>Edytuj</button>
+                        </div>
+                                <div className="col-xs-1 btrecipeadmin"> 
+                            <button className="btn btn-danger" onClick={(event) => 
+                            window.confirm("Czy napewno chcesz usunąć ten przepis?") &&
+                            this.props.removeFromInventory(this.props.book.name)
+                            }>Usuń
+                            </button>
+                        </div>
+                        </div>
+                    </AccordionItemTitle>
+                <AccordionItemBody>
+                    <div>
+                        <div class="col-md-4 btrecipeadmin">{this.props.book.image}</div>
+                        <div class="col-md-8 btrecipeadmin"><strong>Składniki</strong><br/>
+                        <div class="descriptioncomponents">{this.props.book.componentsRecipe}</div></div>
+                    </div>  
                     <div class="col-md-12">
-                    <strong>Opis przygotowania</strong><br/>
-                    <div class="descriptioncomponents">
-                    {this.props.book.preparationDescription}</div>
+                        <strong>Opis przygotowania</strong><br/>
+                        <div class="descriptioncomponents">{this.props.book.preparationDescription}</div>
                     </div>
-                    </div> 
-                </div>
-               <div className="col-md-12">
-               <div className="col-xs-2 btrecipeadmin">
-                    <button className="btn btn-danger" onClick={ (event) => this.props.sendBookToEdit(this.props.book)}>Edytuj</button>
-                </div>
-                <div className="col-xs-2 btrecipeadmin">
-                    <button className="btn btn-danger" onClick={
-                         (event) => 
-                         window.confirm("Czy napewno chcesz usunąć ten przepis?") &&
-                         this.props.removeFromInventory(this.props.book.name)
-                         }>Usuń</button>
-                </div>
-               </div>
-                
+                 </AccordionItemBody>
+                </AccordionItem>
+            </Accordion>
             </div>
                 
         );
